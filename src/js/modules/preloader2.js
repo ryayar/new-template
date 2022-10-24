bd2780c6c445ae20fd85fce486310f36d0d58451
ryayar <89771789+ryayar@users.noreply.github.com>
2022-10-25 00:51:14 +0400

    4 Commit

--- src/js/modules/preloader2.js
+++ src/js/modules/preloader2.js
@@ -1,25 +1,32 @@
-let
-	images = document.images,
-	images_total_count = images.length,
-	images_load_count = 0,
-	preloader = document.getElementById('page-preloader'),
-	loader = document.getElementById('loader'),
-	perc_display = document.getElementById('load_perc'),
-	i = 0;
+const init = () => {
+	const
+		images = document.images,
+		images_total_count = images.length;
+	// preloader = document.getElementById('page-preloader'),
+	// loader = document.getElementById('loader'),
+	// perc_display = document.getElementById('load_perc');
+
+	let
+		images_load_count = 0,
+		i = 0;
 
 // while (i < images_total_count) {
-images[0].src.onload = function () {
-	console.log(images[0]);
+	images[0].onload = image_loaded;
+	images[0].onerror = image_loaded;
 	// images_load_count++;
 	// perc_display.innerHTML = (((100 / images_total_count) * images_load_count) << 0) + '%';
 	// $(loader).css("width", perc_display.innerHTML);
-	}
+
 	// image_clone.onload = image_loaded;
 
 	// image_clone.onerror = image_loaded;
 	// image_clone.src = images[i].src;
 // 	i++
 // }
+
+	function image_loaded() {
+		console.log('Привет')
+	}
 
 // function image_loaded() {
 // 	images_load_count++;
@@ -35,7 +42,8 @@ images[0].src.onload = function () {
 // 	// 	}, 1000);
 // 	// }
 // }
+};
 
-// export default {
-// 	init
-// };
+export default {
+	init,
+};
