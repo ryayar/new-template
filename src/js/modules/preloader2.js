415582ac6e7999989054c5c9bf52fd264493c455
ryayar <89771789+ryayar@users.noreply.github.com>
2022-10-25 01:14:08 +0400

    Change logic

--- src/js/modules/preloader2.js
+++ src/js/modules/preloader2.js
@@ -1,47 +1,37 @@
 const init = () => {
 	const
 		images = document.images,
-		images_total_count = images.length;
-	// preloader = document.getElementById('page-preloader'),
-	// loader = document.getElementById('loader'),
-	// perc_display = document.getElementById('load_perc');
+		images_total_count = images.length,
+		// preloader = document.getElementById('page-preloader'),
+		loader = document.getElementById('loader'),
+		perc_display = document.getElementById('load_perc');
 
 	let
-		images_load_count = 0,
-		i = 0;
+		images_load_count = 0;
 
-// while (i < images_total_count) {
-	images[0].onload = image_loaded;
-	images[0].onerror = image_loaded;
-	// images_load_count++;
-	// perc_display.innerHTML = (((100 / images_total_count) * images_load_count) << 0) + '%';
-	// $(loader).css("width", perc_display.innerHTML);
-
-	// image_clone.onload = image_loaded;
-
-	// image_clone.onerror = image_loaded;
-	// image_clone.src = images[i].src;
-// 	i++
-// }
-
-	function image_loaded() {
-		console.log('Привет')
+	for ( let i = 0;i < images_total_count; i++) {
+		let image_clone = new Image();
+		image_clone.onload = image_loaded
+		image_clone.onerror = image_loaded
+		image_clone.src = images[i].src
+		console.log(i)
 	}
 
-// function image_loaded() {
-// 	images_load_count++;
-// 	perc_display.innerHTML = (((100 / images_total_count) * images_load_count) << 0) + '%';
-// 	$(loader).css("width", perc_display.innerHTML);
-// 	// console.log('perc_display.innerHTML ' + perc_display.innerHTML);
-//
-// 	// if (images_load_count >= images_total_count) {
-// 	// 	setTimeout(function () {
-// 	// 		if (!preloader.classList.contains('done')) {
-// 	// 			preloader.classList.add('done')
-// 	// 		}
-// 	// 	}, 1000);
-// 	// }
-// }
+	function image_loaded() {
+		images_load_count++;
+		perc_display.innerHTML = (((100 / images_total_count) * images_load_count) << 0) + '%';
+		$(loader).css("width", perc_display.innerHTML);
+		console.log('perc_display.innerHTML');
+		console.log(perc_display.innerHTML);
+
+		// if (images_load_count >= images_total_count) {
+		// 	setTimeout(function () {
+		// 		if (!preloader.classList.contains('done')) {
+		// 			preloader.classList.add('done')
+		// 		}
+		// 	}, 1000);
+		// }
+	}
 };
 
 export default {
